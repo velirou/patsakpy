a35842c5373b2101595e64a14501b0420927c7e3
Veli <velirou@gmail.com>
2016-09-25 15:20:41 +0300

    Thema Septemvriou 2016

---
+++ thema7.py
@@ -0,0 +1,17 @@
+#uncomment everything to check functionality
+
+
+def isSquare(x):
+	sqrt = x**(.5)
+	if sqrt%1 == 0.0:
+		#print "it is true"
+		return True
+	#print "it is not false"
+	return False
+
+
+#x = int(raw_input())
+
+#isSquare(x)
+
+
